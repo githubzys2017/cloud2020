18ddd400a5c3b24e1a6d5c38e2750d1cf267c209
zhangyongshuai <yongshuai.zhang@52ydwf.com>
2020-05-29 09:41:13 +0800

    创建cloud消费者

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: 张永帅
+ * @Description:
+ * @Date: Create in 8:45 2020/5/29
+ */
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class, args);
+    }
+}
---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: 张永帅
+ * @Description:
+ * @Date: Create in 8:50 2020/5/29
+ */
+@RestController
+@Slf4j
+public class OrderController {
+
+}
