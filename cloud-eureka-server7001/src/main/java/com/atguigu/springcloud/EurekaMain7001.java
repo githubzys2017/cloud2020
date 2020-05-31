8e56c252b1a8f34aea71ce0ffe6fb1c9c92c347e
zhangyongshuai <yongshuai.zhang@52ydwf.com>
2020-05-31 16:29:19 +0800

    添加Eureka服务注册配置,服务注册,服务发现

--- cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -2,18 +2,17 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author: 张永帅
  * @Description:
- * @Date: Create in 23:41 2020/5/29
+ * @Date: Create in 9:27 2020/5/30
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
-
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author: 张永帅
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: Create in 22:57 2020/5/28
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
