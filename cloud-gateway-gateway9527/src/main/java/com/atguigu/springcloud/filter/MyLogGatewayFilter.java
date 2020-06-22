4bccbb75fa24bd3801695c4aa76e047047feb5cb
zhangyongshuai <yongshuai.zhang@52ydwf.com>
2020-06-22 23:12:04 +0800

    gateway,过滤器

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/filter/MyLogGatewayFilter.java
@@ -0,0 +1,37 @@
+package com.atguigu.springcloud.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * @Author: 张永帅
+ * @Description:
+ * @Date: Create in 22:41 2020/6/22
+ */
+@Component
+public class MyLogGatewayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("******** come in MyLogGatewayFilter: " + new Date());
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (null == uname) {
+            System.out.println("************** 用户名为null，非法用户");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
---
+++ cloud-gateway-gateway9527/src/test/java/com/atguigu/test/ZonedDateTimeDemo.java
@@ -0,0 +1,15 @@
+package com.atguigu.test;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @Author: 张永帅
+ * @Description:
+ * @Date: Create in 21:44 2020/6/22
+ */
+public class ZonedDateTimeDemo {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+    }
+}
