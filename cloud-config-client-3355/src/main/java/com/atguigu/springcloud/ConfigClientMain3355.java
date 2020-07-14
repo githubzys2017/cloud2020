e57ae3c647a352b4ab0043fd16bf7b6bb1cf6bfe
zhangyongshuai <yongshuai.zhang@52ydwf.com>
2020-07-14 21:34:01 +0800

    spring-config 配置

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: 张永帅
+ * @Description:
+ * @Date: Create in 21:04 2020/7/14
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: 张永帅
+ * @Description:
+ * @Date: Create in 21:08 2020/7/14
+ */
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
